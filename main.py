3057bdc80dc6207b1f85a4383cf21e847b38a474
Erick Santiago <erickgalindosc@gmail.com>
2026-06-30 03:24:53 -0500

    creacion modelo clientes y dos end point (listar todos y uno con crear clientes)

--- main.py
+++ main.py
@@ -1,8 +1,39 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-#endpoint
-@app.get("/")
-def inicio():
-    return{"mensaje:" "hola estoy aprendiendo fastapi"}
+lista_clientes: list[cliente] =[]
+
+class cliente(BaseModel):
+    id: int
+    nombre: str
+    email : str
+    descripcion: str
+
+
+
+#(id, nombre , email, descripcion)
+
+#endpoint los clientes, obtener
+@app.get("/clientes")
+def listar_clientes():
+    return lista_clientes
+
+
+#endpoint un solo cliente
+@app.get("/cliente/{cliente_id}")
+def listar_cliente(cliente_id: int):
+    #recorrer a lista clientes
+    for i, obj_cliente in enumerate(lista_clientes):
+        if obj_cliente.get("id")== cliente_id:
+            return obj_cliente
+        
+
+#endpoint para crear un cliente y agregar a la lista
+
+@app.post("/clientes")
+def crear_cliente(datos_cliente: cliente):
+    lista_clientes.append(datos_cliente)
+    return datos_cliente
+   
